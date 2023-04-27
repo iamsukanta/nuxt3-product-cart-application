3abd1c7850301738669d8fb21f4304501619ebd7
iamsukanta <sukantabalacste2016@gmail.com>
2023-04-27 09:55:51 +0600

    product details page updated

---
+++ pages/products/[id].vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Product details for {{ id }}</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio voluptates totam tempora, optio laboriosam libero vitae non aliquid consequuntur. Dicta.</p>
+  </div>
+</template>
+
+<script setup>
+  const { id } = useRoute().params;
+</script>
+
+<style scoped>
+
+</style>
