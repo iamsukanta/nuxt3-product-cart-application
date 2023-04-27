474ca5e2d5ee5bee444978639dcaed6ea76aba0b
iamsukanta <sukantabalacste2016@gmail.com>
2023-04-27 16:49:47 +0600

    default layout added

---
+++ layouts/default.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <header>
+      <nav>
+        <NuxtLink to="/">Nuxt Product Cart</NuxtLink>
+        <ul>
+          <li><NuxtLink to="/">Home</NuxtLink></li>
+          <li><NuxtLink to="/about">About</NuxtLink></li>
+          <li><NuxtLink to="/products">Products</NuxtLink></li>
+        </ul>
+      </nav>
+    </header>
+    <slot />
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style  scoped>
+  .router-link-exact-active {
+    color: #12b488;
+  }
+</style>
