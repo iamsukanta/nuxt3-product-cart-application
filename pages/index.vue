4ab0d91eaa99ddf1e24a9c28e8cb8a7578d330d6
iamsukanta <sukantabalacste2016@gmail.com>
2023-04-27 09:46:01 +0600

    Nuxt3 initital file added

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h2>Home</h2>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente blanditiis consequatur voluptas illo necessitatibus ipsa ipsam ea inventore quisquam odit!</p>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style  scoped>
+
+</style>
