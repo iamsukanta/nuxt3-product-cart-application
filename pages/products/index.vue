8b457dfb7994c66ccc4061c728ae28c2e039fb79
iamsukanta <sukantabalacste2016@gmail.com>
2023-04-27 09:50:11 +0600

    about page and product index file added

---
+++ pages/products/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h2>Product</h2>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente blanditiis consequatur voluptas illo necessitatibus ipsa ipsam ea inventore quisquam odit!</p>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente blanditiis consequatur voluptas illo necessitatibus ipsa ipsam ea inventore quisquam odit!</p>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style  scoped>
+
+</style>
