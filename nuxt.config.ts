afd02bd6e8ad8d8ea9fd013a9c7853c1dd580fb7
iamsukanta <sukantabalacste2016@gmail.com>
2023-04-28 14:36:01 +0600

    Server dynamic api route added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,6 +11,9 @@ export default defineNuxtConfig({
         {rel: "stylesheet", href: "https://fonts.googleapis.com/icon?family=Material+Icons"}
       ]
     }
+  },
+  runtimeConfig: {
+    currencyKey: process.env.CURRENCY_API_KEY
   }
 })
 
